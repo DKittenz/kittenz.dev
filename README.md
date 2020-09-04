3117ab827cd3e23799ad66f1e5b4b96f85773d3a
Roni Nevalainen <kitten@kittenz.dev>
2020-09-04 22:00:41 +0300

    Deployed initial website

--- README.md
+++
@@ -1,3 +0,0 @@
-# kittenz.dev
-
-My website, nothing special.
---
+++ precache-manifest.ed87c00134f8c606bb27bc3be9429031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae88eb62bfc9d8dad57f6ca13136280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b168a91e34e0407719d9",
+    "url": "/static/css/main.3cdcc2cd.chunk.css"
+  },
+  {
+    "revision": "9c59856041e92eabfddd",
+    "url": "/static/js/2.5128adf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5128adf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b168a91e34e0407719d9",
+    "url": "/static/js/main.87627f7d.chunk.js"
+  },
+  {
+    "revision": "511915e87a7da69cbcfc",
+    "url": "/static/js/runtime-main.8e5ce7d9.js"
+  }
+]);
