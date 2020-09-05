9a19d6349351ec6cce257a23a9c62252b84c645f
Roni Nevalainen <kitten@kittenz.dev>
2020-09-05 08:53:44 +0300

    Added unit tests for App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders coming soon", () => {
 	const { getByText } = render(<App />);
-	const linkElement = getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+	const textElement = getByText(/coming soon/i);
+	expect(textElement).toBeInTheDocument();
+});
+
+test("renders just wait", () => {
+	const { getByText } = render(<App />);
+	const textElement = getByText(/Just wait/i);
+	expect(textElement).toBeInTheDocument();
 });
