976c5fb6d00bd5c9703ab1b55dfc38da229f6a4e
Roni Nevalainen <kitten@kittenz.dev>
2020-09-07 20:32:06 +0300

    Added a link to my portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ let App = () => (
 	<div className="App">
 		<header className="App-header">
 			<p>Website coming soon!</p>
-			<p>Just wait......</p>
+			<p>Meanwhile you can <a href="https://devforum.roblox.com/t/--/728207">visit my portfolio</a>!</p>
 		</header>
 	</div>
 );
