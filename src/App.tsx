f85fab22de358e4bf607f43a59ff42476368a2a6
Roni Nevalainen <kitten@kittenz.dev>
2020-09-11 19:39:31 +0300

    Replaced portfolio with my links section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ let App = () => (
 	<div className="App">
 		<header className="App-header">
 			<p>Website coming soon!</p>
-			<p>Meanwhile you can <a href="https://devforum.roblox.com/t/--/728207">visit my portfolio</a>!</p>
+			<p>My links:</p>
+			<p>| <a href="https://www.roblox.com/users/325114244/profile">Roblox</a> | <a href="https://twitter.com/Dev_Kittenz">Twitter</a> | <a href="https://www.youtube.com/channel/UCkJ3idmj-K2YImynaB_dV5w?sub_confirmation=1">YouTube</a> | <a href="https://devforum.roblox.com/t/--/728207">Roblox Portfolio</a> |</p>
 		</header>
 	</div>
 );
