2cb095b913004076aa403e0a19b1230b5be61b2d
Roni Nevalainen <kitten@kittenz.dev>
2020-09-20 16:09:43 +0300

    Add new material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import React from "react";
+import * as Material from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 import "./App.css";
 
-let App = () => (
-	<div className="App">
-		<header className="App-header">
-			<p>Website coming soon!</p>
-			<p>My links:</p>
-			<p>| <a href="https://www.roblox.com/users/325114244/profile">Roblox</a> | <a href="https://twitter.com/Dev_Kittenz">Twitter</a> | <a href="https://www.youtube.com/channel/UCkJ3idmj-K2YImynaB_dV5w?sub_confirmation=1">YouTube</a> | <a href="https://devforum.roblox.com/t/--/728207">Roblox Portfolio</a> |</p>
-		</header>
-	</div>
-);
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+	mainContainer: {
+		marginTop: "75px",
+	},
+}));
+
+let App = () => {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<Material.AppBar>
+				<Material.Toolbar>
+					<Material.Typography variant="h6" className={classes.title}>
+						Kittenz's website
+					</Material.Typography>
+				</Material.Toolbar>
+			</Material.AppBar>
+			<Material.Container className={classes.mainContainer}>
+				<Material.Typography variant="h5">Hello</Material.Typography>
+			</Material.Container>
+		</div>
+	);
+};
 
 export default App;
